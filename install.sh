88b13c8e6b9546f1fa96742b9b763c00db70df2a
Anurag Nayak <nayakanurag078@gmail.com>
2025-03-20 13:10:13 +0530

    made main.py executable

--- install.sh
+++ install.sh
@@ -7,4 +7,6 @@ echo -e "---------------\n"
 DETAIL="$(whoami) is my name"
 echo -e "$DETAIL \n I live in India"
 
+chmod +x ./main.py
+
 # To make this script executable, run chmod +x /path/to/file/install.sh
