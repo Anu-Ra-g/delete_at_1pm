0131f585879aaa1a3ff5c7d2953574eb2a25a138
Anurag Nayak <nayakanurag078@gmail.com>
2025-03-20 13:03:51 +0530

    added some details

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ echo "Hello World"
 echo -e "---------------\n"
 
 DETAIL="$(whoami) is my name"
+echo -e "$DETAIL \n I live in India"
 
 # To make this script executable, run chmod +x /path/to/file/install.sh
